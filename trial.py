f935cf121f45745be90bca665483a6669c08450f
hopennah <hopepaul2905@gmail.com>
2025-03-12 22:55:51 +0100

    This is my Python assignment

---
+++ trial.py
@@ -0,0 +1,5 @@
+#calculator to compute the sum of two numbers
+Number1 = int(input("Enter the First Number: "))
+Number2 = int(input("Enter the Second Number: "))
+sum = Number1 + Number2
+print("The sum of the two numbers is:" , sum)
